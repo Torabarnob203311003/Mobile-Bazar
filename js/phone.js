a14ea9e959ed6f6a1fa018c05d679a325747479e
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-06-01 16:57:08 +0600

    mobile data loaded and display in ui

--- js/phone.js
+++ js/phone.js
@@ -3,8 +3,36 @@
  const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
  const data = await res.json();
   const phones = data.data;
-  console.log(phones)
+  diplayPhones(phones)
 
 
  }
+  const diplayPhones  = phones => {
+
+    // step 1 : get element by id
+     const phoneContainer= document.getElementById('phone-container')
+//  console.log(phones)
+ phones.forEach( phone =>{
+
+ console.log(phones)
+//   step 2 : create a div 
+  const phoneCard = document.createElement('div')
+//   step 3. set the class 
+  phoneCard.classList =(`card w-96 bg-base-100 shadow-xl`)
+   phoneCard.innerHTML = ` <figure><img src="${phone.image}" alt="Shoes" /></figure>
+<div class="card-body">
+    <h2 class="card-title"> ${phone.phone_name} </h2>
+    <p>${phone.slug}</p>
+    <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+    </div>
+
+   `;
+    phoneContainer.appendChild(phoneCard);
+    
+
+ })
+
+
+  }
   loadPhone()
